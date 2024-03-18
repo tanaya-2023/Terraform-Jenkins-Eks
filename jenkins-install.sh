dac6e05d9abd0867938daa20fd4a459607013c66
tanaya-2023 <87536696+tanaya-2023@users.noreply.github.com>
2024-03-18 22:20:15 +0530

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -31,7 +31,6 @@ sudo apt-get install terraform -y
 
 
 ## Install Kubectl 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubec
-tl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
