ad5c4477806a739b53e4d781dc1e27cf51ad5802
tanaya-2023 <87536696+tanaya-2023@users.noreply.github.com>
2024-03-18 07:40:42 +0000

    Eks folder modified

--- Eks-Install-folder/main.tf
+++ Eks-Install-folder/main.tf
@@ -35,9 +35,9 @@ data "aws_availability_zones" "availability-zones" {
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
-  cluster_endpoint_public_access  = true
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.24"
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
